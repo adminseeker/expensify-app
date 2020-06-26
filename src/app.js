2a31ca8275860bd5d404ff034bd0719c115b5d66
Aravind A <aravind.arali@gmail.com>
2020-06-26 23:20:18 +0530

    configuring and refactoring react components and redux

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import {addExpense} from "./actions/expenses";
+import {setTextFilter} from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses"; 
 import "normalize.css/normalize.css"
 import "./styles/styles.scss";
-import AppRouter from "./routers/AppRouter";
+
+const store = configureStore();
+
+store.dispatch(addExpense({description:"water bill"}));
+store.dispatch(addExpense({description:"gas bill"}));
+
+store.dispatch(setTextFilter("wat"));
+
+const state = store.getState();
+console.log(getVisibleExpenses(state.expenses,state.filters));
 
 ReactDOM.render(<AppRouter /> ,document.getElementById("app"));
