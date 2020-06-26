ebe4d120573dcf15bc6a4e4a248182b989a1b053
Aravind A <aravind.arali@gmail.com>
2020-06-26 19:31:11 +0530

    Added redux filters and sorting and waiting for integration with react-components

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,15 +1,55 @@
-import { createStore, combineReducers } from 'redux';
-
+import { createStore, combineReducers } from "redux";
+import uuid from "uuid";
 
 const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
+    case "ADD_EXPENSE":
+      return [
+        ...state,
+        action.expense
+      ]
+    case "REMOVE_EXPENSE":
+      return state.filter((expense)=>(expense.id!==action.id))
+
+    case "EDIT_EXPENSE":
+      return state.map((expense)=>{
+        if(expense.id===action.id){
+          return {
+            ...expense,
+            ...action.updates
+          }
+        }else{
+          return expense;
+        }
+      })
     default:
       return state;
   }
 };
 
+const addExpense = ({description="",note="",amount=0,createdAt=0}={})=>({
+    type:"ADD_EXPENSE",
+    expense:{
+      id:uuid(),
+      description,
+      note,
+      amount,
+      createdAt
+    }
+});
+
+const removeExpense = ({id})=>({
+  type:"REMOVE_EXPENSE",
+  id
+})
+
+const editExpense = (id,updates)=>({
+  type:"EDIT_EXPENSE",
+  id,
+  updates
+})
 
 const filtersReducerDefaultState = {
   text: '',
@@ -20,11 +60,74 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
   switch (action.type) {
+    case "SET_TEXT_FILTER":
+      return {
+        ...state,
+        text:action.text
+      }
+    case "SORT_BY_AMOUNT":
+      return {
+        ...state,
+        sortBy:"amount"
+      }
+      case "SORT_BY_DATE":
+        return {
+          ...state,
+          sortBy:"date"
+        }
+      case "SET_START_DATE":
+        return{
+          ...state,
+          startDate:action.startDate
+        }
+      case "SET_END_DATE":
+        return{
+          ...state,
+          endDate:action.endDate
+        }  
     default:
       return state;
   }
 };
 
+const setTextFilter = (text="")=>({
+  type:"SET_TEXT_FILTER",
+  text
+})
+
+const sortByAmount = ()=>({
+  type:"SORT_BY_AMOUNT"
+})
+
+const sortByDate = ()=>({
+  type:"SORT_BY_DATE"
+})
+
+const setStartDate = (startDate)=>({
+  type:"SET_START_DATE",
+  startDate
+})
+
+const setEndDate = (endDate)=>({
+  type:"SET_END_DATE",
+  endDate
+})
+
+const getVisisbleExpenses = (expenses,{text,sortBy,startDate,endDate})=>{
+  return expenses.filter((expense)=>{
+    const startDateMatch = typeof startDate !=="number" || expense.createdAt>=startDate;
+    const endDateMatch = typeof endDate !=="number" || expense.createdAt<=endDate;
+    const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+    return startDateMatch && endDateMatch && textMatch;
+  }).sort((a,b)=>{
+    if(sortBy==="date"){
+      return a.createdAt < b.createdAt ? 1 : -1
+    }else if(sortBy==="amount"){
+      return a.amount < b.amount ? 1 : -1;
+    }
+  })
+}
 
 const store = createStore(
   combineReducers({
@@ -33,7 +136,27 @@ const store = createStore(
   })
 );
 
-console.log(store.getState());
+store.subscribe(()=>{
+  const state = store.getState();
+  const visibleExpenses = getVisisbleExpenses(state.expenses,state.filters);
+  console.log(visibleExpenses);
+})
+
+const expenseOne = store.dispatch(addExpense({note:"rent",amount:100,description:"rent"}));
+const expenseTwo = store.dispatch(addExpense({note:"coffee",amount:10,description:"coffee"}));
+
+// store.dispatch(removeExpense({id:expenseOne.expense.id}));
+// store.dispatch(editExpense(expenseTwo.expense.id,{amount:20}));
+
+store.dispatch(setTextFilter("e"));
+// store.dispatch(setTextFilter());
+
+// store.dispatch(sortByAmount());
+// store.dispatch(sortByDate());
+
+// store.dispatch(setStartDate("125"));
+// // store.dispatch(setStartDate());
+// store.dispatch(setEndDate("126"));
 
 const demoState = {
   expenses: [{
@@ -50,3 +173,4 @@ const demoState = {
     endDate: undefined
   }
 };
+
