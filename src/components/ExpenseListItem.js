6376aa44111d683221c76eaf0fb1ba416dc9ac9d
Aravind A <aravind.arali@gmail.com>
2020-06-28 18:04:50 +0530

    Added date and currency formatting on dashboard

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,14 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import moment from "moment";
+import numeral from "numeral";
 
 const ExpenseListItem = (props)=>(
     <div>
         <Link to={"/edit/"+props.expense.id}>
         <h3>{props.expense.description}</h3>
         </Link>
-        <p>{props.expense.amount} - {props.expense.createdAt}</p>
+        <p>₹{numeral(props.expense.amount/100).format("0,0.00")} - {moment(props.expense.createdAt).format("MMMM Do, YYYY")}</p>
     </div>
 );
 
