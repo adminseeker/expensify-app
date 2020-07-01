1f6ce9da2bb7d5dc7b9ae5e7d0202d55f3622f79
Aravind A <aravind.arali@gmail.com>
2020-07-01 22:06:11 +0530

    Added styles to ExpenseForm

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -6,17 +6,24 @@ import { startEditExpense, startRemoveExpense } from "../actions/expenses";
 const EditExpensePage = (props) => {
   return (
     <div>
+      <div className="page-header">
+        <div className="content-container">
+          <h1 className="page-header__title">Edit Expense</h1>
+        </div>
+      </div>
+      <div className="content-container">
       <ExpenseForm
-        expense={props.expense}
-        onSubmit={(expense) => {
-          props.dispatch(startEditExpense(props.expense.id, expense));
-          props.history.push("/");
-        }}
-      />
-      <button onClick={() => {
-        props.dispatch(startRemoveExpense({ id: props.expense.id }));
+      expense={props.expense}
+      onSubmit={(expense) => {
+        props.dispatch(startEditExpense(props.expense.id, expense));
         props.history.push("/");
-      }}>Remove</button>
+      }}
+    />
+    <button className="button button--secondary" onClick={() => {
+      props.dispatch(startRemoveExpense({ id: props.expense.id }));
+      props.history.push("/");
+    }}>Remove</button>
+      </div>
     </div>
   );
 };
