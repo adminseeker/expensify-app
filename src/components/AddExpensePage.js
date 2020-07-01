bd9bdb492691b1fc52b74d4d385e626a84847a8b
Aravind A <aravind.arali@gmail.com>
2020-07-02 00:11:05 +0530

    fixed a bug in AddExpensePage Component

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -5,14 +5,21 @@ import {startAddExpense} from "../actions/expenses";
 
 const AddExpensePage = (props)=>(
     <div>
-        <h1>Add Expense</h1>
-        <ExpenseForm 
+        <div className="page-header">
+          <div className="content-container">
+            <h1 className="page-header__title">Add Expense</h1>
+          </div>
+        </div>
+        <div className="content-container">
+          <ExpenseForm
             onSubmit={(expense)=>{
-                props.dispatch(startAddExpense(expense));
-                props.history.push("/");
+            props.dispatch(startAddExpense(expense));
+            props.history.push("/");
             }}
-        />
-    </div>
+          />
+        </div>
+      </div>
 );
 
 export default connect()(AddExpensePage);
+
