0929f89315a22a47fcb6ec1e76d7899ebc6f6171
Aravind A <aravind.arali@gmail.com>
2020-07-01 23:51:59 +0530

    Added LoadingPage with loading gif ans some styles for it

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,7 +5,7 @@ import {startLogin} from "../actions/auth";
 const LoginPage = (props)=>(
     <div className="box-layout">
         <div className="box-layout__box">
-          <h1 className="box-layout__title">Expensify App</h1>
+          <h1 className="box-layout__title">Expensify</h1>
             <p>It's time to get your expenses under control.</p>
             <button 
                 className="button"
