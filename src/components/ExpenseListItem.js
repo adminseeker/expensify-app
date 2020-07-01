1df8d2419bb8258d913c374a97d2a1e84e7f7e08
Aravind A <aravind.arali@gmail.com>
2020-07-01 22:51:05 +0530

    Added styles to ExpenseListItem component

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -6,10 +6,10 @@ import numeral from "numeral";
 const ExpenseListItem = (props)=>(
     <Link className="list-item" to={"/edit/"+props.expense.id}>
     <div>
-        <h3>{props.expense.description}</h3>
-        <span>{moment(props.expense.createdAt).format('MMMM Do, YYYY')}</span>
+        <h3 className="list-item__title" >{props.expense.description}</h3>
+        <span className="list-item__sub-title" >{moment(props.expense.createdAt).format('MMMM Do, YYYY')}</span>
     </div>
-    <h3>₹{numeral(props.expense.amount/100).format("0,0.00")}</h3>
+    <h3 className="list-item__data" >₹{numeral(props.expense.amount/100).format("0,0.00")}</h3>
     </Link>
 );
 
